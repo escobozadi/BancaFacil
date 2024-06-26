17008355b24d7eea963823ff120f6ce8560dc680
escobozadi <dianaescoboza@gmail.com>
2024-06-25 21:31:29 -0400

    Initial Commit

---
+++ BancaFacil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BancaFacil
+//
+//  Created by Diana Escoboza on 2024-06-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
